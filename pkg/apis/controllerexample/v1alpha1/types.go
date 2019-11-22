9c16bab09b74ac93d19b275fe13f392677ef98b7
jinghongtai <676621871@qq.com>
2019-11-22 18:00:44 +0800

    1111

---
+++ pkg/apis/controllerexample/v1alpha1/types.go
@@ -0,0 +1,38 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Foo is a specification for a Foo resource
+type App struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   AppSpec   `json:"spec"`
+	Status AppStatus `json:"status"`
+}
+
+// FooSpec is the spec for a Foo resource
+type AppSpec struct {
+	DeploymentName string `json:"deploymentName"`
+	Replicas       *int32 `json:"replicas"`
+}
+
+// FooStatus is the status for a Foo resource
+type AppStatus struct {
+	AvailableReplicas int32 `json:"availableReplicas"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// FooList is a list of Foo resources
+type AppList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+
+	Items []App `json:"items"`
+}
